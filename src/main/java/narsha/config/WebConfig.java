f482647b264e9843478791c9a1e7a28546a02fb5
yuhano <jonglee32@naver.com>
2024-08-04 04:33:02 +0900

    [feat] web config 추가 Access-Control-Allow-Origin 해더 수정 코드 추가

---
+++ src/main/java/narsha/config/WebConfig.java
@@ -0,0 +1,19 @@
+package narsha.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해
+                .allowedOrigins("http://3.36.127.16:3000") // 허용할 오리진 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
+                .allowedHeaders("*") // 모든 헤더 허용
+                .allowCredentials(true) // 쿠키를 포함할지 설정
+                .maxAge(3600); // 프리플라이트 응답의 캐시 지속 시간(초)
+    }
+}

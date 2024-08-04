057a89cce1ed6ff95aed015141eff6fc65e015c6
yuhano <jonglee32@naver.com>
2024-08-05 01:04:38 +0900

    [fix] allowedOrigins 주소 수정 및 권한 변경

--- src/main/java/narsha/config/WebConfig.java
+++ src/main/java/narsha/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
+                        "http://nurspace-narsha.duckdns.org",
                         "http://nurspace-narsha.duckdns.org:3000",
                         "http://localhost:3000"
 
